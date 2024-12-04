362a147399aeb7f6cbd53e4e3f69107a90f19b09
Robert Ford <robert.ford.29.ctr@us.af.mil>
2024-12-04 12:33:03 -0600

    add initial Switchconfigs.js file

---
+++ Switchconfigs.js
@@ -0,0 +1,328 @@
+login as: robert.ford.adm
+Pre-authentication banner message from server:
+| C
+|    ***************************************************************
+|                        ###  #####  ###  ####  #
+|                       #   #   #   #   # #   # #
+|                       #       #   #   # #   # #
+|                        ###    #   #   # ####  #
+|                           #   #   #   # #     #
+|                       #   #   #   #   # #
+|                        ###    #    ###  #     #
+|    ***************************************************************
+|    ###############################################################
+|    # THIS IS A DOD COMPUTER SYSTEM.  THIS COMPUTER SYSTEM,       #
+|    # INCLUDING ALL RELATED EQUIPMENT, NETWORKS & NETWORK DEVICES #
+|    # (SPECIFICALLY INCLUDING INTERNET ACCESS) ARE PROVIDED ONLY  #
+|    # FOR AUTHORIZED U.S. GOVERNMENT USE. DOD COMPUTER SYSTEMS    #
+|    # MAY BE MONITORED FOR ALL LAWFUL PURPOSES, FOR MANAGEMENT    #
+|    # OF THE SYSTEM, TO FACILATE PROTECTION, AND TO VERIFY        #
+|    # SECURITY PROCEDURES, SURVIVABILITY, AND OPSEC. MONITORING   #
+|    # INCLUDES ACTIVE ATTACKS BY AUTHORIZED DOD ENTITES. DURING   #
+|    # MONITORING, INFORMATION MAY BE EXAMINED, RECORDED, COPIED,  #
+|    # AND USED FOR AUTHORIZED PURPOSES.  ALL INFORMATION,         #
+|    # INCLUDING PERSONAL INFORMATION MAY BE MONITORED. USE OF     #
+|    # THIS DOD COMPUTER SYSTEM, AUTHORIZED OR UNAUTHORIZED,       #
+|    # CONSTITUTES CONSENT TO MONITORING OF THIS SYSTEM. EVIDENCE  #
+|    # OF UNAUTHORIZED USE COLLECTED DURING MONITORING MAY BE USED #
+|    # FOR ADMINISTRATIVE, CRIMINAL OR OTHER ADVERSE ACTIONS.  USE #
+|    # OF THIS SYSTEM CONSTITUTES CONSENT TO MONITORING FOR THESE  #
+|    # PURPOSES.                                                   #
+|    ###############################################################
+End of banner message from server
+Keyboard-interactive authentication prompts from server:
+| Password:
+End of keyboard-interactive prompts from server
+
+TPSA-SW-OOBM8-3#
+TPSA-SW-OOBM8-3#
+TPSA-SW-OOBM8-3#sh int status
+
+Port      Name               Status       Vlan       Duplex  Speed Type
+Gi1/0/1   OOBM connection    connected    110        a-full a-1000 10/100/1000BaseTX
+Gi1/0/2   OOBM connection    connected    110        a-full a-1000 10/100/1000BaseTX
+Gi1/0/3   OOBM connection    connected    110        a-full a-1000 10/100/1000BaseTX
+Gi1/0/4   OOBM connection    connected    110        a-full a-1000 10/100/1000BaseTX
+Gi1/0/5   OOBM connection    connected    110        a-full a-1000 10/100/1000BaseTX
+Gi1/0/6   OOBM connection    connected    110        a-full a-1000 10/100/1000BaseTX
+Gi1/0/7   OOBM connection    connected    110        a-full a-1000 10/100/1000BaseTX
+Gi1/0/8   OOBM connection    connected    110        a-full a-1000 10/100/1000BaseTX
+Gi1/0/9   OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/10  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/11  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/12  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/13  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/14  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/15  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/16  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/17  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/18  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/19  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/20  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/21  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/22  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/23  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/24  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/25  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/26  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/27  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/28  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/29  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/30  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/31  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/32  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/33  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/34  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/35  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/36  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/37  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/38  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/39  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/40  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/41  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/42  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/43  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/44  OOBM connection    connected    110        a-full  a-100 10/100/1000BaseTX
+Gi1/0/45  OOBM connection    connected    110        a-full  a-100 10/100/1000BaseTX
+Gi1/0/46  OOBM connection    connected    110        a-full a-1000 10/100/1000BaseTX
+Gi1/0/47  OOBM connection    connected    110        a-full  a-100 10/100/1000BaseTX
+Gi1/0/48  ::IXIA Access Swit connected    103        a-full a-1000 10/100/1000BaseTX
+
+Port      Name               Status       Vlan       Duplex  Speed Type
+Gi1/0/49  OOBM connection    notconnect   666          auto   auto Not Present
+Gi1/0/50  OOBM connection    notconnect   666          auto   auto Not Present
+Gi1/0/51  OOBM connection    notconnect   666          auto   auto Not Present
+Gi1/0/52  UPLINK to Top of R connected    trunk      a-full a-1000 1000BaseSX SFP
+TPSA-SW-OOBM8-3#  sh vlan brief
+
+VLAN Name                             Status    Ports
+---- -------------------------------- --------- -------------------------------
+1    default                          active
+103  Server MGMT                      active    Gi1/0/48
+110  CVAH OOBM                        active    Gi1/0/1, Gi1/0/2, Gi1/0/3, Gi1/0/4, Gi1/0/5, Gi1/0/6, Gi1/0/7, Gi1/0/8, Gi1/0/9, Gi1/0/10, Gi1/0/11, Gi1/0/12, Gi1/0/13, Gi1/0/14
+                                                Gi1/0/15, Gi1/0/16, Gi1/0/17, Gi1/0/18, Gi1/0/19, Gi1/0/20, Gi1/0/21, Gi1/0/22, Gi1/0/23, Gi1/0/24, Gi1/0/25, Gi1/0/26, Gi1/0/27
+                                                Gi1/0/28, Gi1/0/29, Gi1/0/30, Gi1/0/31, Gi1/0/32, Gi1/0/33, Gi1/0/34, Gi1/0/35, Gi1/0/36, Gi1/0/37, Gi1/0/38, Gi1/0/39, Gi1/0/40
+                                                Gi1/0/41, Gi1/0/42, Gi1/0/43, Gi1/0/44, Gi1/0/45, Gi1/0/46, Gi1/0/47
+111  Network MGMT                     active
+112  VLAN0112                         active
+666  UNUSED PORT                      active    Gi1/0/49, Gi1/0/50, Gi1/0/51
+999  NATIVE                           active
+1002 fddi-default                     act/unsup
+1003 token-ring-default               act/unsup
+1004 fddinet-default                  act/unsup
+1005 trnet-default                    act/unsup
+TPSA-SW-OOBM8-3#
+TPSA-SW-OOBM8-3#
+TPSA-SW-OOBM8-3#config t
+Enter configuration commands, one per line.  End with CNTL/Z.
+TPSA-SW-OOBM8-3(config)#
+TPSA-SW-OOBM8-3(config)#vlan 112
+TPSA-SW-OOBM8-3(config-vlan)#name UPS/PDU MGMT
+TPSA-SW-OOBM8-3(config-vlan)#end
+TPSA-SW-OOBM8-3#wr
+Building configuration...
+[OK]
+TPSA-SW-OOBM8-3#  sh vlan brief
+
+VLAN Name                             Status    Ports
+---- -------------------------------- --------- -------------------------------
+1    default                          active
+103  Server MGMT                      active    Gi1/0/48
+110  CVAH OOBM                        active    Gi1/0/1, Gi1/0/2, Gi1/0/3, Gi1/0/4, Gi1/0/5, Gi1/0/6, Gi1/0/7, Gi1/0/8, Gi1/0/9, Gi1/0/10, Gi1/0/11, Gi1/0/12, Gi1/0/13, Gi1/0/14
+                                                Gi1/0/15, Gi1/0/16, Gi1/0/17, Gi1/0/18, Gi1/0/19, Gi1/0/20, Gi1/0/21, Gi1/0/22, Gi1/0/23, Gi1/0/24, Gi1/0/25, Gi1/0/26, Gi1/0/27
+                                                Gi1/0/28, Gi1/0/29, Gi1/0/30, Gi1/0/31, Gi1/0/32, Gi1/0/33, Gi1/0/34, Gi1/0/35, Gi1/0/36, Gi1/0/37, Gi1/0/38, Gi1/0/39, Gi1/0/40
+                                                Gi1/0/41, Gi1/0/42, Gi1/0/43, Gi1/0/44, Gi1/0/45, Gi1/0/46, Gi1/0/47
+111  Network MGMT                     active
+112  UPS/PDU MGMT                     active
+666  UNUSED PORT                      active    Gi1/0/49, Gi1/0/50, Gi1/0/51
+999  NATIVE                           active
+1002 fddi-default                     act/unsup
+1003 token-ring-default               act/unsup
+1004 fddinet-default                  act/unsup
+1005 trnet-default                    act/unsup
+TPSA-SW-OOBM8-3#sh int status
+
+Port      Name               Status       Vlan       Duplex  Speed Type
+Gi1/0/1   OOBM connection    connected    110        a-full a-1000 10/100/1000BaseTX
+Gi1/0/2   OOBM connection    connected    110        a-full a-1000 10/100/1000BaseTX
+Gi1/0/3   OOBM connection    connected    110        a-full a-1000 10/100/1000BaseTX
+Gi1/0/4   OOBM connection    connected    110        a-full a-1000 10/100/1000BaseTX
+Gi1/0/5   OOBM connection    connected    110        a-full a-1000 10/100/1000BaseTX
+Gi1/0/6   OOBM connection    connected    110        a-full a-1000 10/100/1000BaseTX
+Gi1/0/7   OOBM connection    connected    110        a-full a-1000 10/100/1000BaseTX
+Gi1/0/8   OOBM connection    connected    110        a-full a-1000 10/100/1000BaseTX
+Gi1/0/9   OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/10  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/11  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/12  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/13  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/14  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/15  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/16  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/17  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/18  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/19  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/20  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/21  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/22  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/23  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/24  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/25  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/26  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/27  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/28  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/29  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/30  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/31  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/32  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/33  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/34  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/35  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/36  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/37  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/38  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/39  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/40  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/41  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/42  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/43  OOBM connection    notconnect   110          auto   auto 10/100/1000BaseTX
+Gi1/0/44  OOBM connection    connected    110        a-full  a-100 10/100/1000BaseTX
+Gi1/0/45  OOBM connection    connected    110        a-full  a-100 10/100/1000BaseTX
+Gi1/0/46  OOBM connection    connected    110        a-full a-1000 10/100/1000BaseTX
+Gi1/0/47  OOBM connection    connected    110        a-full  a-100 10/100/1000BaseTX
+Gi1/0/48  ::IXIA Access Swit connected    103        a-full a-1000 10/100/1000BaseTX
+
+Port      Name               Status       Vlan       Duplex  Speed Type
+Gi1/0/49  OOBM connection    notconnect   666          auto   auto Not Present
+Gi1/0/50  OOBM connection    notconnect   666          auto   auto Not Present
+Gi1/0/51  OOBM connection    notconnect   666          auto   auto Not Present
+Gi1/0/52  UPLINK to Top of R connected    trunk      a-full a-1000 1000BaseSX SFP
+TPSA-SW-OOBM8-3#config t
+Enter configuration commands, one per line.  End with CNTL/Z.
+TPSA-SW-OOBM8-3(config)#int range gi1/0/45 - 47
+TPSA-SW-OOBM8-3(config-if-range)#switc
+TPSA-SW-OOBM8-3(config-if-range)#switchport acc
+TPSA-SW-OOBM8-3(config-if-range)#switchport access vlan 112
+TPSA-SW-OOBM8-3(config-if-range)#end
+TPSA-SW-OOBM8-3#wr
+Building configuration...
+[OK]
+TPSA-SW-OOBM8-3#sh run
+TPSA-SW-OOBM8-3#sh running-config int gi1/0/45
+Building configuration...
+
+Current configuration : 423 bytes
+!
+interface GigabitEthernet1/0/45
+ description OOBM connection
+ switchport access vlan 112
+ switchport mode access
+ switchport block unicast
+ mac access-group MacBlock in
+ storm-control broadcast level bps 62m
+ storm-control multicast level bps 62m
+ storm-control unicast level bps 62m
+ storm-control action trap
+ spanning-tree portfast edge
+ spanning-tree bpduguard enable
+ spanning-tree guard root
+ ip verify source
+end
+
+TPSA-SW-OOBM8-3#sh running-config int gi1/0/46
+Building configuration...
+
+Current configuration : 423 bytes
+!
+interface GigabitEthernet1/0/46
+ description OOBM connection
+ switchport access vlan 112
+ switchport mode access
+ switchport block unicast
+ mac access-group MacBlock in
+ storm-control broadcast level bps 62m
+ storm-control multicast level bps 62m
+ storm-control unicast level bps 62m
+ storm-control action trap
+ spanning-tree portfast edge
+ spanning-tree bpduguard enable
+ spanning-tree guard root
+ ip verify source
+end
+
+TPSA-SW-OOBM8-3#sh running-config int gi1/0/47
+Building configuration...
+
+Current configuration : 423 bytes
+!
+interface GigabitEthernet1/0/47
+ description OOBM connection
+ switchport access vlan 112
+ switchport mode access
+ switchport block unicast
+ mac access-group MacBlock in
+ storm-control broadcast level bps 62m
+ storm-control multicast level bps 62m
+ storm-control unicast level bps 62m
+ storm-control action trap
+ spanning-tree portfast edge
+ spanning-tree bpduguard enable
+ spanning-tree guard root
+ ip verify source
+end
+
+TPSA-SW-OOBM8-3#sh mac add
+TPSA-SW-OOBM8-3#sh mac address-table int gi1/0/45
+          Mac Address Table
+-------------------------------------------
+
+Vlan    Mac Address       Type        Ports
+----    -----------       --------    -----
+ 112    00c0.b7ec.892f    DYNAMIC     Gi1/0/45
+Total Mac Addresses for this criterion: 1
+TPSA-SW-OOBM8-3#sh mac address-table int gi1/0/46
+          Mac Address Table
+-------------------------------------------
+
+Vlan    Mac Address       Type        Ports
+----    -----------       --------    -----
+ 112    2829.8651.15a6    DYNAMIC     Gi1/0/46
+Total Mac Addresses for this criterion: 1
+TPSA-SW-OOBM8-3#sh mac address-table int gi1/0/47
+          Mac Address Table
+-------------------------------------------
+
+Vlan    Mac Address       Type        Ports
+----    -----------       --------    -----
+ 112    00c0.b78f.dbd4    DYNAMIC     Gi1/0/47
+Total Mac Addresses for this criterion: 1
+TPSA-SW-OOBM8-3#sh running-config int gi1/0/44
+Building configuration...
+
+Current configuration : 423 bytes
+!
+interface GigabitEthernet1/0/44
+ description OOBM connection
+ switchport access vlan 110
+ switchport mode access
+ switchport block unicast
+ mac access-group MacBlock in
+ storm-control broadcast level bps 62m
+ storm-control multicast level bps 62m
+ storm-control unicast level bps 62m
+ storm-control action trap
+ spanning-tree portfast edge
+ spanning-tree bpduguard enable
+ spanning-tree guard root
+ ip verify source
+end
+
+TPSA-SW-OOBM8-3#config t
+Enter configuration commands, one per line.  End with CNTL/Z.
+TPSA-SW-OOBM8-3(config)#interface GigabitEthernet1/0/44
+TPSA-SW-OOBM8-3(config-if)#switchport access vlan 112
+TPSA-SW-OOBM8-3(config-if)#end
+TPSA-SW-OOBM8-3#wr
+Building configuration...
+[OK]
+TPSA-SW-OOBM8-3#
